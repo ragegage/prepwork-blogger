cc47933f4746db1403b004b150a7c451afc56b5e
Gage <gagesnewman@gmail.com>
2016-03-03 13:12:07 -0800

    Tagging feature completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303010648) do
+ActiveRecord::Schema.define(version: 20160303011910) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,22 @@ ActiveRecord::Schema.define(version: 20160303010648) do
     t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
